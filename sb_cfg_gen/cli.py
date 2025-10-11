391d14ee789a4442684f9e5dae737a42ff815c87
Jacko <l2982211930@163.com>
2025-10-11 18:28:31 +0800

    feat(cli): 从命令行获取链接

--- sb_cfg_gen/cli.py
+++ sb_cfg_gen/cli.py
@@ -1,5 +1,22 @@
+import argparse
+import json
+from sb_cfg_gen.lib import url_get_raw_config_file
+from sb_cfg_gen.lib import patch_config_file
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="订阅链接")
+args = parser.parse_args()
+
+
 def main():
-    print("Hello, sing-box config generator!!!")
+    url = args.url
+    print(f"正在获取: {url}")
+    raw = url_get_raw_config_file(url)
+    cfg_file = patch_config_file(raw)
+    with open(f"config.json", "w") as f:
+        json.dump(cfg_file, f, indent=4, ensure_ascii=False)
+    print("保存成功！")
 
 
 if __name__ == "__main__":
