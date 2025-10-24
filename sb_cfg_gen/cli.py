3b418845d8d083e39abba4e87e3f45e4404f3d2b
Jacko <l2982211930@163.com>
2025-10-25 00:19:57 +0800

    feat(cli): 改成引导获取 url

--- sb_cfg_gen/cli.py
+++ sb_cfg_gen/cli.py
@@ -1,20 +1,14 @@
-import argparse
 import json
 from sb_cfg_gen.lib import url_get_raw_config_file
 from sb_cfg_gen.lib import patch_config_file
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("url", help="订阅链接")
-args = parser.parse_args()
-
-
 def main():
-    url = args.url
+    url = input("url: ")
     print(f"正在获取: {url}")
     raw = url_get_raw_config_file(url)
     cfg_file = patch_config_file(raw)
-    with open(f"config.json", "w") as f:
+    with open(f"jacko.json", "w") as f:
         json.dump(cfg_file, f, indent=4, ensure_ascii=False)
     print("保存成功！")
 
