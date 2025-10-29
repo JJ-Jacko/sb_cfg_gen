bb93ce9594cd432b449e1d02585547e09f9c4ffb
Jacko <l2982211930@163.com>
2025-10-29 11:13:23 +0800

    rafactor(libs/node): 迁移 node 节点相关的代码

--- sb_cfg_gen/libs/other.py
+++ sb_cfg_gen/libs/other.py
@@ -1,6 +1,5 @@
 import json
 from typing import List
-from typing import Literal
 from pathlib import Path
 
 
@@ -13,93 +12,6 @@ def keywords_in_text(
             return True
     
     return False
-
-
-def extra_nodes_from_singbox_config(singbox_config: dict) -> List[dict]:
-    node_types = [
-        "hysteria2",
-        "shadowsocks",
-        "vless",
-        "vmess",
-    ]
-    airport_info_keywords = [
-        # KTM
-        "剩余流量",
-        "距离下次重置剩余",
-        "套餐到期",
-        
-        # 流量光
-        "剩余流量",
-        "距离下次重置流量剩余",
-        "未到期"
-    ]
-    
-    nodes = []
-    for outbound in singbox_config["outbounds"]:
-        # 过滤 代理组
-        if outbound["type"] not in node_types:
-            continue
-
-        # 过滤 机场信息
-        if keywords_in_text(airport_info_keywords, outbound["tag"]):
-            continue
-        
-        nodes.append(outbound)
-        
-    return nodes
-
-
-def node_in_nodes(node: dict, nodes: List[dict]):
-    for exist_node in nodes:
-        if (
-            node["server"] == exist_node["server"]
-            and node["server_port"] == exist_node["server_port"]
-        ):
-            return True
-
-    return False
-
-
-def nodes_deduplicate(nodes: List[dict]) -> List[dict | None]:
-    cleaned_nodes = []
-    for n in nodes:
-        if node_in_nodes(n, cleaned_nodes):
-            continue
-        
-        cleaned_nodes.append(n)
-
-    return cleaned_nodes
-
-
-def filter_nodes_with_specified_area(
-        nodes: List[dict],
-        area: Literal["HK", "TW", "SG", "JP", "US", "Other"]
-) -> List[dict | None]:
-    map = {
-        "HK": ["香港", "Hong Kong"],
-        "TW": ["台湾", "Taiwan"],
-        "SG": ["新加坡", "Singapore"],
-        "JP": ["日本", "Japan"],
-        "US": ["美国", "United States"],
-        "Other": [""]
-    }
-    
-    if area == "Other":
-        return [
-            node
-            for node in nodes
-            if not keywords_in_text(map.get("HK"), node["tag"])
-            if not keywords_in_text(map.get("TW"), node["tag"])
-            if not keywords_in_text(map.get("SG"), node["tag"])
-            if not keywords_in_text(map.get("JP"), node["tag"])
-            if not keywords_in_text(map.get("US"), node["tag"])
-        ]
-    
-    return [
-        node
-        for node in nodes
-        if keywords_in_text(map.get(area), node["tag"])
-    ]
 
 
 def patch_config_file(
