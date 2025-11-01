20e1eb88c1ccae5bb77fb23133a814a09e27a881
Jacko <l2982211930@163.com>
2025-11-01 21:59:02 +0800

    feat(libs/node/merge_singbox_config): 合成最终配置文件

--- sb_cfg_gen/libs/node.py
+++ sb_cfg_gen/libs/node.py
@@ -1,4 +1,6 @@
 import copy
+import json
+from pathlib import Path
 from typing import List
 from typing import Literal
 from typing import get_args
@@ -130,3 +132,109 @@ def rename_same_area_nodes(
     
     return renamed_nodes
 
+
+def merge_singbox_config(
+        hq_HK_nodes: List[Node],
+        hq_TW_nodes: List[Node],
+        hq_SG_nodes: List[Node],
+        hq_JP_nodes: List[Node],
+        hq_US_nodes: List[Node],
+        hq_other_nodes: List[Node],
+        lq_HK_nodes: List[Node],
+        lq_TW_nodes: List[Node],
+        lq_SG_nodes: List[Node],
+        lq_JP_nodes: List[Node],
+        lq_US_nodes: List[Node],
+        lq_other_nodes: List[Node],
+        with_clash_api: bool = True,
+        template_file: Path = Path("template.json")
+):
+    with template_file.open("r") as f:
+        template: SingBoxConfig = json.load(f)
+    
+    if with_clash_api:
+        template["experimental"]["clash_api"] = {
+            "external_controller": "0.0.0.0:9090",
+            "external_ui": "dashboard"
+        }
+
+    # 代理组
+    template["outbounds"].extend([
+        {
+            "type": "selector",
+            "tag": "🖐️ Manual",
+            "outbounds": [
+                n["tag"]
+                for n in hq_HK_nodes + lq_HK_nodes + \
+                        hq_TW_nodes + lq_TW_nodes + \
+                        hq_SG_nodes + lq_SG_nodes + \
+                        hq_JP_nodes + lq_JP_nodes + \
+                        hq_US_nodes + lq_US_nodes + \
+                        hq_other_nodes + lq_other_nodes
+            ]
+        },
+        {
+            "type": "urltest",
+            "tag": "⚒️ DataSaver",
+            "outbounds": [
+                n["tag"]
+                for n in lq_HK_nodes + lq_TW_nodes + \
+                        lq_SG_nodes + lq_JP_nodes + \
+                        lq_US_nodes + \
+                        lq_other_nodes
+            ]
+        },
+        {
+            "type": "urltest",
+            "tag": "🇭🇰 HK",
+            "outbounds": [
+                n["tag"]
+                for n in hq_HK_nodes
+            ]
+        },
+        {
+            "type": "urltest",
+            "tag": "🇹🇼 TW",
+            "outbounds": [
+                n["tag"]
+                for n in hq_TW_nodes
+            ]
+        },
+        {
+            "type": "urltest",
+            "tag": "🇸🇬 SG",
+            "outbounds": [
+                n["tag"]
+                for n in hq_SG_nodes
+            ]
+        },
+        {
+            "type": "urltest",
+            "tag": "🇯🇵 JP",
+            "outbounds": [
+                n["tag"]
+                for n in hq_JP_nodes
+            ]
+        },
+        {
+            "type": "urltest",
+            "tag": "🇺🇸 US",
+            "outbounds": [
+                n["tag"]
+                for n in hq_US_nodes
+            ]
+        }
+    ])
+
+    # 节点
+    template["outbounds"].extend([
+        n
+        for n in hq_HK_nodes + lq_HK_nodes + \
+                hq_TW_nodes + lq_TW_nodes + \
+                hq_SG_nodes + lq_SG_nodes + \
+                hq_JP_nodes + lq_JP_nodes + \
+                hq_US_nodes + lq_US_nodes + \
+                hq_other_nodes + lq_other_nodes
+    ])
+    
+    return template
