9eb4f0560004977c2289318a78fd8805cdcd0750
Jacko <l2982211930@163.com>
2026-03-21 23:17:28 +0800

    fix(NodeFactor.filter_nodes_with_specified_area): 通过清理 tag 的类型的关键词达到修复被垃圾信息干扰导致的将干扰信息判定为地区信息从而将节点添加到不同的地区组的 bug

--- src/sb_cfg_gen/libs/node_factor.py
+++ src/sb_cfg_gen/libs/node_factor.py
@@ -9,6 +9,25 @@ from sb_cfg_gen.libs.types import AreaCode
 
 
 class NodeFactor:
+    node_type_keywords: List[str] = [
+        "[优]",
+        "[家宽]",
+        "[家宽/住宅]",
+        "[原生]",
+        "[非原生]",
+        "[日动]",
+        "[月动]",
+        "[大宽带]",
+        "[星链]",
+        "[starlink]",
+        "[T-Mobile]",
+        "[So-Net]",
+        "[Hawaiian]",
+        "[ISP]",
+        "[ATT]",
+        "[GTT]"
+    ]
+    
     @classmethod
     def __node_in_nodes(cls, node: Node, nodes: List[Node]):
         for exist_node in nodes:
@@ -19,6 +38,17 @@ class NodeFactor:
                 return True
 
         return False
+
+    @classmethod
+    def __get_cleaned_tag(cls, tag: str):
+        tag_cleaned = tag
+
+        for kw in cls.node_type_keywords:
+            i = tag_cleaned.find(kw)
+            if i != -1:
+                tag_cleaned = tag_cleaned[:i] + tag_cleaned[i + len(kw):]
+        
+        return tag_cleaned
 
     @classmethod
     def deduplicate_nodes(cls, nodes: List[Node]) -> List[Node | None]:
@@ -40,14 +70,14 @@ class NodeFactor:
         
         filtered_nodes: List[Node] = []
         for node in nodes:
-            tag = node["tag"]
-            if Areas.get(area_code).flag in tag:
+            tag_cleaned = cls.__get_cleaned_tag(node["tag"])
+            if Areas.get(area_code).flag in tag_cleaned:
                 filtered_nodes.append(node)
-            elif area_code in tag:
+            elif area_code in tag_cleaned:
                 filtered_nodes.append(node)
-            elif area_code == "IN" and "印度尼西亚" in tag:
+            elif area_code == "IN" and "印度尼西亚" in tag_cleaned:
                 continue
-            elif keywords_in_text(Areas.get(area_code).keywords, tag):
+            elif keywords_in_text(Areas.get(area_code).keywords, tag_cleaned):
                 filtered_nodes.append(node)
 
         return filtered_nodes
