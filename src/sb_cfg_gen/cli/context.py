509f59bd223ed33dc3374488d51c107102d7221f
Jacko <l2982211930@163.com>
2026-06-21 16:19:08 +0800

    remove(cli.context): not using path

--- src/sb_cfg_gen/cli/context.py
+++ src/sb_cfg_gen/cli/context.py
@@ -5,31 +5,19 @@ from sb_cfg_gen.other import load_config
 
 __all__ = [
     # Path
-    "nodes_file",
+    "project_config_file",
     "raw_cfg_file",
-    "airport_app_file",
-    "airport_cli_file",
-    "airport_server_file",
-    "diy_app_file",
-    "diy_cli_file",
-    "diy_server_file"
+    "nodes_file",
+    "nodes_diy_file"
 ]
 
 # Path
 project_config_file = Path("config.toml")
 cache_dir = Path("cache")
-output_dir = Path("output")
 raw_cfg_file = cache_dir/"raw_cfg.json"
 nodes_file = cache_dir/"nodes.json"
 nodes_diy_file = cache_dir/"nodes_diy.json"
-airport_app_file = output_dir/"airport-app.json"
-airport_cli_file = output_dir/"airport-cli.json"
-airport_server_file = output_dir/"airport-server.json"
-diy_app_file = output_dir/"diy-app.json"
-diy_cli_file = output_dir/"diy-cli.json"
-diy_server_file = output_dir/"diy-server.json"
 
 # Initialization
 project_config = load_config(project_config_file)
 cache_dir.mkdir(parents=True, exist_ok=True)
-output_dir.mkdir(parents=True, exist_ok=True)
